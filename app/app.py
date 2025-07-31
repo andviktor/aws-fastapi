5fdc6ad9874aa95769370321f827dd4cecdaaa28
Viktor Andreev <andviktor@Air-de-Viktor.fritz.box>
2025-07-31 11:43:57 +0200

    Logger test

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-import uvicorn
 import platform
 import sys
+from logging import Logger
 
 app: FastAPI = FastAPI()
+logger: Logger = Logger(__name__)
 
 @app.get("/")
 async def root() -> dict:
+    logger.error("LOGGER TEST!")
     return {
         "python_version": platform.python_version(),
         "interpreter_path": sys.executable
