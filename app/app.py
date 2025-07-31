5de417d9182b7e7a6e2d2c5617cf0313a28ec1f4
Viktor Andreev <andviktor@Air-de-Viktor.fritz.box>
2025-07-31 11:52:20 +0200

    secret test

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import platform
 import sys
+import os
 from logging import Logger
 
 app: FastAPI = FastAPI()
@@ -8,7 +9,8 @@ logger: Logger = Logger(__name__)
 
 @app.get("/")
 async def root() -> dict:
-    logger.error("LOGGER TEST!")
+    TEST_SECRET = os.getenv("TEST_SECRET", "NO SECRET")
+    logger.error(f"TEST_SECRET: {TEST_SECRET}")
     return {
         "python_version": platform.python_version(),
         "interpreter_path": sys.executable
